ef61449560433a87f15de50decc1a6850b61ee3a
Tomas V <jerry.tm@seznam.cz>
2021-11-17 10:30:20 +0000

    chakra + chakra templates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
+import type { ReactElement, ReactNode } from 'react'
+import Layout from '../components/Layout'
+import { ChakraProvider } from "@chakra-ui/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export function defaultLayout(page: ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || defaultLayout
+  return (
+    <ChakraProvider>
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  );
+}
+
+export default App
