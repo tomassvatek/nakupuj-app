6b44dce498efe04c28b31f0f3ccaee141e0dd271
Lukáš Litvan <lukas.litvan@gmail.com>
2021-11-21 15:38:29 +0100

    Add responsiveness to the slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -10,8 +10,26 @@ const Slider = ({ ...props }: Swiper) => {
     <Swiper
       modules={[Scrollbar]}
       spaceBetween={10}
-      slidesPerView={5}
+      slidesPerView={1}
       scrollbar={{ draggable: true, hide: true }}
+      breakpoints={{
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        800: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 20
+        },
+        1920: {
+          slidesPerView: 5,
+          spaceBetween: 30
+        }
+      }}
     >
       {products.map((product) => (
         <SwiperSlide key={product.id}>
