12c982b428663990438d6ff222cb61aee8a1a516
pospit13 <pospit13@cvut>
2021-11-22 21:36:12 +0100

    payment

--- pages/cart/payment.tsx
+++ pages/cart/payment.tsx
@@ -1,12 +1,26 @@
 import { Box, Heading } from '@chakra-ui/layout'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react'
 import BreadcrumbComponent from '../../components/Breadcrumb'
-import CallToActionWithAnnotation from '../../components/Hero'
 import { getTitle } from '../../utils/getTitle'
+import {Address} from "./delivery/address";
+import {PaymentMethod} from "./delivery/paymentMethod";
+import {Modal, useDisclosure} from "@chakra-ui/react";
+import {Button} from "@chakra-ui/button/src/button";
+import {
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay
+} from "@chakra-ui/modal/src/modal";
 
 const Payment: NextPage = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <main>
       <Head>
@@ -16,7 +30,34 @@ const Payment: NextPage = () => {
       <Box p={4}>
         <BreadcrumbComponent items={['index', 'cart-payment']} />
         <Heading>Doprava a platba</Heading>
+          <Address/>
+          <PaymentMethod totalAmount={123}/>
       </Box>
+
+        <button onClick={onOpen}>Open Modal</button>
+
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <span>
+                        Děkujeme za nákup.
+                        Souhrn objednávky Vám odešleme na email.
+
+                        Děkujeme
+                    </span>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                        Close
+                    </Button>
+                    <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
     </main>
   )
 }
