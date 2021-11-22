632481c01f6c3eed0df392c322dbd5fd73d9a27c
pospit13 <pospit13@cvut>
2021-11-22 21:58:14 +0100

    payments

--- pages/cart/payment.tsx
+++ pages/cart/payment.tsx
@@ -1,65 +1,32 @@
-import { Box, Heading } from '@chakra-ui/layout'
-import type { NextPage } from 'next'
+import {Box, Heading} from '@chakra-ui/layout'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import React from 'react'
 import BreadcrumbComponent from '../../components/Breadcrumb'
-import { getTitle } from '../../utils/getTitle'
+import {getTitle} from '../../utils/getTitle'
 import {Address} from "./delivery/address";
 import {PaymentMethod} from "./delivery/paymentMethod";
-import {Modal, useDisclosure} from "@chakra-ui/react";
-import {Button} from "@chakra-ui/button/src/button";
-import {
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay
-} from "@chakra-ui/modal/src/modal";
 
 const Payment: NextPage = () => {
+    return (
+        <main>
+            <Head>
+                <title>{getTitle('Doprava a platba')}</title>
+            </Head>
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-  return (
-    <main>
-      <Head>
-        <title>{getTitle('Doprava a platba')}</title>
-      </Head>
-
-      <Box p={4}>
-        <BreadcrumbComponent items={['index', 'cart-payment']} />
-        <Heading>Doprava a platba</Heading>
-          <Address/>
-          <PaymentMethod totalAmount={123}/>
-      </Box>
-
-        <button onClick={onOpen}>Open Modal</button>
-
-        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-                <ModalHeader>Modal Title</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    <span>
-                        Děkujeme za nákup.
-                        Souhrn objednávky Vám odešleme na email.
-
-                        Děkujeme
-                    </span>
-                </ModalBody>
-
-                <ModalFooter>
-                    <Button colorScheme="blue" mr={3} onClick={onClose}>
-                        Close
-                    </Button>
-                    <Button variant="ghost">Secondary Action</Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    </main>
-  )
+            <Box p={4}>
+                <BreadcrumbComponent items={['index', 'cart-payment']}/>
+                <Heading>Doprava a platba</Heading>
+                <Address/>
+                <PaymentMethod totalAmount={123}/>
+                <Box style={{display: 'flex', justifyContent: 'center'}}>
+                    <div style={{width: '60%', padding: '0 2rem', display: 'flex', justifyContent: 'flex-end'}}>
+                        <a href="https://nakupuj-app.vercel.app/" style={{padding: '15px 32px', border: 'none', backgroundColor: '#EDF2F7', borderRadius: '5px'}}>Dokončit nákup</a>
+                    </div>
+                </Box>
+            </Box>
+        </main>
+    )
 }
 
 export default Payment
