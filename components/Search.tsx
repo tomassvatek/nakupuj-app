208fa87db783ccc0ba357b00238e0aaa9f49a56f
Tomáš Svatek <svatektomas117@gmail.com>
2021-11-23 20:31:15 +0100

    Search result z-index

--- components/Search.tsx
+++ components/Search.tsx
@@ -11,25 +11,21 @@ import {
   useMenuButton,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
-import { products } from '../constants';
-import ProductVariantItem from './ProductVariantItem';
+import { products } from "../constants";
+import ProductVariantItem from "./ProductVariantItem";
 
 // eslint-disable-next-line react/display-name
 const MenuPlacer: any = React.forwardRef<any>(
   ({ children, as: Component, ...rest }: any, ref) => {
     const buttonProps = useMenuButton(rest, ref);
 
-    return (
-      <Component {...buttonProps}>
-        {children}
-      </Component>
-    );
+    return <Component {...buttonProps}>{children}</Component>;
   }
-)
+);
 
 export function Search() {
   const ref = useRef<HTMLInputElement>(null);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const product = products[0];
   const variants = products[0].variants;
 
@@ -44,22 +40,32 @@ export function Search() {
   };
 
   const handleSelect = () => {
-    setQuery('');
-  }
+    setQuery("");
+  };
 
   return (
-    <Menu isOpen={true} placement={'auto'} computePositionOnMount>
+    <Menu isOpen={true} placement={"auto"} computePositionOnMount>
       <MenuPlacer as={InputGroup}>
         <InputLeftElement pointerEvents="none">
           <SearchIcon />
         </InputLeftElement>
-        <Input type="search" placeholder="Hledat" background="white" value={query} onChange={handleQuery} ref={ref} />
+        <Input
+          type="search"
+          placeholder="Hledat"
+          background="white"
+          value={query}
+          onChange={handleQuery}
+          ref={ref}
+        />
       </MenuPlacer>
 
       {query ? (
-        <MenuList>
+        <MenuList zIndex="200">
           {variants.map((variant) => (
-            <NextLink href={`/product/${product.id}/${variant.id}`} key={variant.id}>
+            <NextLink
+              href={`/product/${product.id}/${variant.id}`}
+              key={variant.id}
+            >
               <MenuItem as={Flex} onClick={handleSelect}>
                 <ProductVariantItem variant={variant} />
               </MenuItem>
@@ -68,5 +74,5 @@ export function Search() {
         </MenuList>
       ) : null}
     </Menu>
-  )
+  );
 }
