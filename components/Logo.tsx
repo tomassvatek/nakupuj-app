8effce1dfb4e4d228646ed936549903eec21ff3d
Tomas V <jerry.tm@seznam.cz>
2021-11-23 16:54:32 +0000

    logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 import { Image } from "@chakra-ui/react";
 
 const Logo = forwardRef<SVGSVGElement>((props, ref) => {
-  return <Image src="logo.svg" alt="Logo" width="160px" />;
+  return <Image src="/logo.svg" alt="Logo" width="160px" />;
 });
 
 Logo.displayName = "Logo";
