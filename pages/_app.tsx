c222c2e0596b7df0166599dceea3e67bbd4fbe7c
Tomas V <jerry.tm@seznam.cz>
2021-12-07 22:18:19 +0000

    progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
-import type { ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode, useEffect } from 'react'
 import Layout from '../components/Layout'
 import { ChakraProvider } from "@chakra-ui/react"
 import { CartProvider } from '../hooks/useCart'
 import nakupujTheme from '../theme';
+// @ts-ignore
+import NProgress from 'nprogress'
+import { useRouter } from 'next/router'
+import '../public/progress.css'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -25,6 +29,27 @@ export function defaultLayout(page: ReactElement) {
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || defaultLayout
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
     <ChakraProvider theme={nakupujTheme}>
       <CartProvider>
