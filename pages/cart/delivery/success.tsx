5b753279953560cbaf146f7df65a0bd552bf66a6
Tomáš Svatek <svatektomas117@gmail.com>
2021-12-07 23:30:14 +0100

    Add message for user after complete the order.

--- pages/cart/delivery/success.tsx
+++ pages/cart/delivery/success.tsx
@@ -1,26 +1,31 @@
-import {Box, Button, Heading, Text} from '@chakra-ui/react';
-import { CheckCircleIcon } from '@chakra-ui/icons';
-import type {NextPage} from "next";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+import type { NextPage } from "next";
 import NextLink from "next/link";
 import React from "react";
 
-const Success:NextPage =  () => {
-    return (
-        <Box textAlign="center" py={10} px={6}>
-            <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
-            <Heading as="h2" size="xl" mt={6} mb={2}>
-                Nákup byl úspěšně dokončen
-            </Heading>
-            <Text color={'gray.500'}>
-                Děkujeme Vám za nákup na portálu Nakupuj.cz. <br/> Vaši objednávku předáme dodavateli a bude u Vás v zadaný čas.
-            </Text>
-            <div style={{paddingTop: '2rem'}}>
-                <NextLink href="/">
-                    <Button size="lg">Zpět na hlavní stranu</Button>
-                </NextLink>
-            </div>
-        </Box>
-    );
-}
+const Success: NextPage = () => {
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <CheckCircleIcon boxSize={"50px"} color={"green.500"} />
+      <Heading as="h2" size="xl" mt={6} mb={2}>
+        Nákup byl úspěšně dokončen
+      </Heading>
+      <Text color={"gray.500"}>
+        Děkujeme Vám za nákup na portálu Nakupuj.cz. <br /> Vaši objednávku
+        předáme dodavateli a bude u Vás v zadaný čas. <br />
+      </Text>
+      <Text pt="5" color="gray.500">
+        Brzy Vám dorazí e-mail, kde Vaši objednávku potvrdíme. Kurýr Vás bude
+        informovat před doručením sms zprávou.
+      </Text>
+      <div style={{ paddingTop: "2rem" }}>
+        <NextLink href="/">
+          <Button size="lg">Zpět na hlavní stranu</Button>
+        </NextLink>
+      </div>
+    </Box>
+  );
+};
 
 export default Success;
